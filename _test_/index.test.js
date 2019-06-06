d847f5b6064dddc9def4999c4a471051c82cbe46
pnwjce <elliott.jess@gmail.com>
2019-06-05 19:26:20 -0700

    trying to get travis to build

--- _test_/index.test.js
+++ _test_/index.test.js
@@ -5,11 +5,11 @@ let pol = require('../pol.js');
 describe('proof of life', () => {
 
     it('lives by default', () => {
-        expect(pol.isAlive()).toBeTruthy();
+       expect(pol.isAlive()).toBeTruthy();
     });
 
     it('dies with a param', () => {
-        expect(pol.isAlive('x')).toBeFalsy();
+       expect(pol.isAlive('x')).toBeFalsy();
     });
 
 });
